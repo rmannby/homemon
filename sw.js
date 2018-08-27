fb2cfb7c344336f60500846150321720480643ae
Roger Mannby <roger@rm-eltech.com>
2018-08-27 18:52:09 +0200

    Update of service worker and caching

--- sw.js
+++ sw.js
@@ -1,29 +1,8 @@
-const CACHE_STATIC_NAME = 'static-v6'
+const CACHE_STATIC_NAME = 'static-v1'
 const CACHE_DYNAMIC_NAME = 'dynamic-v1';
 
-// const STATIC_FILES = [
-//     '/',
-//     'index.html',
-//     'src/js/app.js',
-//     'src/css/style.css',
-//     'src/images/homemon-48x48.png',
-//     'src/images/homemon-96x96.png',
-//     'src/images/homemon-144x144.png',
-//     'src/images/homemon-192x192.png',
-//     'src/images/homemon-256x256.png',
-//     'src/images/homemon-384x384.png',
-//     'src/images/homemon-512x512.png',
-//     'src/images/temperatureOutside.png',
-//     'src/images/temperatureInside.png',
-//     'src/images/swimmingPool.png',
-//     'src/images/sun.png',
-//     'src/images/weather/10.png',
-//     'src/images/weather/11.png',
-//     'src/images/weather/22.png',
-//     'src/images/weather/30.png'
-// ]
 const STATIC_FILES = [
-    '/',
+    '.',
     'index.html',
     'src/js/app.js',
     'src/css/style.css',
@@ -50,7 +29,7 @@ self.addEventListener('install', function (event) {
         caches.open(CACHE_STATIC_NAME)
         .then(function (cache) {
             console.log('[Service Worker] Precaching App Shell');
-            cache.addAll(STATIC_FILES);
+            return cache.addAll(STATIC_FILES);
         })
     )
 });
@@ -71,7 +50,16 @@ self.addEventListener('activate', function (event) {
     return self.clients.claim();
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function (event) {
+    // console.log('Fetch event for ', event.request.url);
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+/* self.addEventListener('fetch', function(event) {
   event.respondWith(
     fetch(event.request)
       .then(function(res) {
@@ -86,7 +74,7 @@ self.addEventListener('fetch', function(event) {
       })
   );
 });
-
+ */
 // self.addEventListener('fetch', function (event) {
 //     event.respondWith(
 //         caches.match(event.request)
