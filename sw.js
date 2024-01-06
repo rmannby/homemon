4edc74ceeace20bea4b3cfccc31d67e9f8cfa047
Roger Mannby <roger@rm-eltech.com>
2024-01-06 15:57:16 +0100

    Add documentation

--- sw.js
+++ sw.js
@@ -1,3 +1,15 @@
+/**
+ * Defines cache names and static files to precache. 
+ * Registers service worker install, activate and fetch event handlers.
+ * 
+ * CACHE_STATIC_NAME - Name of the static file cache.
+ * CACHE_DYNAMIC_NAME - Name of the dynamic cache. 
+ * STATIC_FILES - List of static files to precache.
+ * 
+ * The install handler opens the static file cache and adds the static files. 
+ * The activate handler deletes old caches.
+ * The fetch handler serves cached responses first, falling back to the network.
+*/
 const CACHE_STATIC_NAME = 'static-v1'
 const CACHE_DYNAMIC_NAME = 'dynamic-v1';
 
@@ -24,33 +36,48 @@ const STATIC_FILES = [
     'src/images/weather/30.png'
 ]
 
+/**
+ * Install event handler for the service worker. 
+ * Logs a message, opens the static file cache, 
+ * and adds the static files to the cache.
+*/
 self.addEventListener('install', function (event) {
     console.log('[Service Worker] Installing Service Worker ...', event);
     event.waitUntil(
         caches.open(CACHE_STATIC_NAME)
-        .then(function (cache) {
-            console.log('[Service Worker] Precaching App Shell');
-            return cache.addAll(STATIC_FILES);
-        })
+            .then(function (cache) {
+                console.log('[Service Worker] Precaching App Shell');
+                return cache.addAll(STATIC_FILES);
+            })
     )
 });
 
+/**
+ * Activate event handler for the service worker. 
+ * Logs a message, deletes old caches, and claims any open clients.
+*/
 self.addEventListener('activate', function (event) {
     console.log('[Service Worker] Activating Service Worker ....', event);
     event.waitUntil(
         caches.keys()
-        .then(function (keyList) {
-            return Promise.all(keyList.map(function (key) {
-                if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
-                    console.log('[Service Worker] Removing old cache.', key);
-                    return caches.delete(key);
-                }
-            }));
-        })
+            .then(function (keyList) {
+                return Promise.all(keyList.map(function (key) {
+                    if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
+                        console.log('[Service Worker] Removing old cache.', key);
+                        return caches.delete(key);
+                    }
+                }));
+            })
     );
     return self.clients.claim();
 });
 
+/**
+ * Installs a fetch event handler that responds to fetch requests 
+ * by first checking the cache for a matching response, and if not found, 
+ * fetching the request from the network. This allows pages to be served
+ * from the cache when offline.  
+*/
 self.addEventListener('fetch', function (event) {
     // console.log('Fetch event for ', event.request.url);
     event.respondWith(
